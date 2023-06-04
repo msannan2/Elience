5add0191f2236a44a99935b225217b200c0a195f
Sannan Nasir <msannan2@gmail.com>
2023-06-04 14:27:53 +0500

    fixes

--- app/src/main/java/com/HCI/elience/QuestionDetailActivity.java
+++ app/src/main/java/com/HCI/elience/QuestionDetailActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
